acbbeb81db3736585d930aa938ba1c340e26f105
brunoverpet <bverpet46@gmail.com>
2026-01-16 17:40:14 +0100

    feat: implement interactive table component with dynamic seating and selection

---
+++ inertia/components/TableEntity.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import type { TableDto } from '#dtos/table_dto'
+
+const props = defineProps<{
+  isSelected: boolean
+  table: TableDto
+}>()
+
+const emit = defineEmits<{
+  (e: 'select', id: number): void
+}>()
+</script>
+
+<template>
+  <div
+    @click="$emit('select', table.id)"
+    class="flex w-full h-16 relative items-center justify-center rounded-2xl"
+    :class="isSelected ? 'bg-teal-400' : 'bg-gray-200'"
+  >
+    <div class="absolute -top-3 w-full flex justify-evenly">
+      <div
+        v-for="n in (table.seats - 2) / 2"
+        :key="n"
+        class="w-8 h-2 bg-gray-300 rounded-full"
+      ></div>
+    </div>
+    <div class="absolute -bottom-3 w-full flex justify-evenly">
+      <div
+        v-for="n in (table.seats - 2) / 2"
+        :key="n"
+        class="w-8 h-2 bg-gray-300 rounded-full"
+      ></div>
+    </div>
+    <div class="absolute -left-3 w-2 h-8 bg-gray-300 rounded-full"></div>
+    <div class="absolute -right-3 w-2 h-8 bg-gray-300 rounded-full"></div>
+    {{ table.number }}
+  </div>
+</template>
--- inertia/pages/rooms/show.vue
+++ inertia/pages/rooms/show.vue
@@ -2,16 +2,40 @@
 import { Head } from '@inertiajs/vue3'
 import { InferPageProps } from '@adonisjs/inertia/types'
 import type RoomsController from '#controllers/rooms_controller'
+import { ref } from 'vue'
+import TableEntity from '~/components/TableEntity.vue'
 
 defineProps<{
   room: InferPageProps<RoomsController, 'show'>['room']
 }>()
+
+const selectedTableId = ref<number | null>(null)
+
+function handleTableSelect(id: number) {
+  if (selectedTableId.value === id) {
+    selectedTableId.value = null
+  } else {
+    selectedTableId.value = id
+  }
+}
 </script>
 
 <template>
   <Head title="Room" />
   <h1>Room page</h1>
-  <div v-for="table in room.tables" :key="table.id">
-    {{ table }}
+
+  <div class="grid grid-cols-4 gap-10">
+    <div
+      v-for="table in room.tables"
+      :key="table.id"
+      class="p-4 flex items-center justify-center cursor-pointer"
+      :class="{ 'col-span-2': table.seats > 4 }"
+    >
+      <TableEntity
+        :is-selected="selectedTableId === table.id"
+        :table="table"
+        @select="handleTableSelect"
+      />
+    </div>
   </div>
 </template>
