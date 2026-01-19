a1b63498d9519dd2ad5679308f70b70817e8af46
brunoverpet <bverpet46@gmail.com>
2026-01-19 11:25:20 +0100

    refactor: update LockService for table locking with bidirectional release

--- app/services/lock_service.ts
+++ app/services/lock_service.ts
@@ -1,28 +1,42 @@
 import redis from '@adonisjs/redis/services/main'
 
 export class LockService {
-  async acquire(resource: string, ownerId: string, ttl: number) {
-    const script = `return redis.call("set", KEYS[1], ARGV[1], "NX", "EX", ARGV[2])`
+  async acquire(ownerId: string, resource: string, ttl: number) {
+    const ownerAcquired = await this.getOwner(ownerId)
 
-    const result = await redis.eval(script, 1, resource, ownerId, ttl)
+    if (ownerAcquired) {
+      await this.release(ownerId, ownerAcquired)
+    }
+
+    const script = `
+    if redis.call("exists", KEYS[1]) == 1 or redis.call("exists", KEYS[2]) == 1 then
+      return 0
+    end
+    redis.call("set", KEYS[1], KEYS[2], "EX", ARGV[1])
+    redis.call("set", KEYS[2], KEYS[1], "EX", ARGV[1])
+    return "OK"`
+
+    const result = await redis.eval(script, 2, ownerId, resource, ttl)
     return result === 'OK'
   }
 
-  async release(resource: string, ownerId: string) {
-    const script = `if redis.call("get", KEYS[1]) == ARGV[1] then
-    return redis.call("del", KEYS[1])
-  else
-    return 0
-  end
+  async release(ownerId: string, resource: string) {
+    const script = `if redis.call("get", KEYS[1]) ~= KEYS[2] or redis.call("get", KEYS[2]) ~= KEYS[1] then
+      return 0
+    end
+    redis.call("del", KEYS[1])
+    redis.call("del", KEYS[2])
+    return "OK"
 `
-    return redis.eval(script, 1, resource, ownerId)
+    const result = await redis.eval(script, 2, ownerId, resource)
+    return result === 'OK'
   }
 
   async isLocked(resource: string) {
     return redis.exists(resource)
   }
 
-  async getOwner(resource: string) {
-    return redis.get(resource)
+  async getOwner(ownerId: string) {
+    return redis.get(ownerId)
   }
 }
