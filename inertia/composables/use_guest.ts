36150789dbe36f7c4d2d56fd66690b68d3ac0ddd
brunoverpet <bverpet46@gmail.com>
2026-01-25 14:52:24 +0100

    fix: use custom random UUID

--- inertia/composables/use_guest.ts
+++ inertia/composables/use_guest.ts
@@ -2,11 +2,21 @@ import { ref } from 'vue'
 
 const guestId = ref<string>('')
 
+// Fonction utilitaire pour générer un UUID sans l'API Crypto
+function generateUUID() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+    const r = (Math.random() * 16) | 0
+    const v = c === 'x' ? r : (r & 0x3) | 0x8
+    return v.toString(16)
+  })
+}
+
 export function useGuest() {
   let storedGuestId = localStorage.getItem('guest_id')
 
   if (!storedGuestId) {
-    storedGuestId = crypto.randomUUID()
+    // On utilise la fonction compatible HTTP
+    storedGuestId = generateUUID()
     localStorage.setItem('guest_id', storedGuestId)
   }
 
--- inertia/libs/transmit.ts
+++ inertia/libs/transmit.ts
@@ -1,5 +1,17 @@
 import { Transmit } from '@adonisjs/transmit-client'
 
+// Une petite fonction qui génère des ID aléatoires sans utiliser crypto
+function generateId() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    const r = (Math.random() * 16) | 0
+    const v = c === 'x' ? r : (r & 0x3) | 0x8
+    return v.toString(16)
+  })
+}
+
 export const transmit = new Transmit({
   baseUrl: window.location.origin,
+
+  // C'est ici qu'on remplace le générateur par défaut qui plante
+  uidGenerator: generateId,
 })
